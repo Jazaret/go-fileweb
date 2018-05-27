0938416087d6dada3b41ade52c4e99cbb8f4df05
Javier Azaret <jazaret@gmail.com>
2018-05-27 10:38:48 -0400

    update HTML content

--- controller/controller.go
+++ controller/controller.go
@@ -10,6 +10,7 @@ const indexPage = "public/index.html"
 
 var (
 	fileController file
+	homeTemplate   *template.Template
 )
 
 //Startup - This is the startup method
@@ -19,14 +20,22 @@ func Startup(templates map[string]*template.Template) {
 	fileController.listTemplate = templates["list.html"]
 	fileController.uploadComplete = templates["complete.html"]
 
+	homeTemplate = templates["home.html"]
+
 	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/favicon.ico", faviconHandler)
 	fileController.registerRoutes()
 
 	http.Handle("/img/", http.FileServer(http.Dir("public")))
 	http.Handle("/css/", http.FileServer(http.Dir("public")))
 }
 
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "public/favicon.ico")
+}
+
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving %s to %s...\n", indexPage, r.RemoteAddr)
-	http.ServeFile(w, r, indexPage)
+	w.Header().Add("Content-Type", "text/html")
+	homeTemplate.Execute(w, nil)
 }
